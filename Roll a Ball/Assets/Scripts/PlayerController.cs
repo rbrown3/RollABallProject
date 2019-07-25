5283a9adb15fd5b794bd6b8704323c3b6c60457d
rbrown3 <rosannabrown@outlook.com>
2019-07-25 08:39:12 -0400

    Roll A Ball Project based on Tutorial

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+
+    private Rigidbody rb;
+    private int count;
+    private AudioSource myAudio;
+
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        myAudio = GetComponent<AudioSource>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        
+    }
+
+    void FixedUpdate()
+    {
+  
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      if (other.gameObject.CompareTag("Pick Up"))
+        {
+             other.gameObject.SetActive(false);
+             count = count + 1;
+             SetCountText();
+             myAudio.Play();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+}
